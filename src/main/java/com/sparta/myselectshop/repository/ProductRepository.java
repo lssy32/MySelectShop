4e0cad3ff9908724fee6de326f165f76dca93d46
lssy32 <lssy32@naver.com>
2023-01-01 21:23:56 +0900

    d

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.myselectshop.repository;
-
-import com.sparta.myselectshop.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
